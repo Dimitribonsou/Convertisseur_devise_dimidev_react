e21cb8efea1541cfa835db24baff7adc096ee0d7
Dimitribonsou <dimitribonsou26@gmail.com>
2024-10-13 19:45:03 +0100

    correction du nom de dev

--- src/Components/HomePage.tsx
+++ src/Components/HomePage.tsx
@@ -4,7 +4,7 @@ import CurrencyConverter from "./form"
 const  HomePage =()=> {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-        <p className='font-bold text-2xl'>Bienvenue sur le convertisseur de devises de Dimi<strong className="text-green-500">Dev</strong></p>
+        <p className='font-bold text-2xl'>Bienvenue sur le convertisseur de devises de <strong className="text-green-500">DimiDev</strong></p>
         <CurrencyConverter/>
     </div>
   )
